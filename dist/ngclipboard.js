bcf6bb4fdac016f9b477580a291d6b96057793be
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-03-17 10:03:21 +1000

    refactor(dist): update built content

--- dist/ngclipboard.js
+++ dist/ngclipboard.js
@@ -1,53 +1,49 @@
-/*! ngclipboard - v2.0.0 - 2018-03-03
+/*! ngclipboard - v3.0.0 - 2021-03-17
 * https://github.com/sachinchoolur/ngclipboard
-* Copyright (c) 2018 Sachin; Licensed MIT */
-(function() {
-    'use strict';
-    var MODULE_NAME = 'ngclipboard';
-    var angular, ClipboardJS;
+* Copyright (c) 2021 Sachin; Licensed MIT */
+import ClipboardJS from 'clipboard';
+const ngclipboard = {
+    register: function(ngModule){
+        ngModule.directive('ngclipboard', function() {
+            return {
+                restrict: 'A',
+                scope: {
+                    ngclipboardSuccess: '&',
+                    ngclipboardError: '&'
+                },
+                link: function(scope, element) {
+                    //constructor for clipboardjs changed to ClipboardJS
+                    var clipboard = new ClipboardJS(element[0]);
 
-    // Check for CommonJS support
-    if (typeof module === 'object' && module.exports) {
-      angular = require('angular');
-      ClipboardJS = require('clipboard');
-      module.exports = MODULE_NAME;
-    } else {
-      angular = window.angular;
-      ClipboardJS = window.ClipboardJS;
-    }
-
-    angular.module(MODULE_NAME, []).directive('ngclipboard', function() {
-        return {
-            restrict: 'A',
-            scope: {
-                ngclipboardSuccess: '&',
-                ngclipboardError: '&'
-            },
-            link: function(scope, element) {
-                //constructor for clipboardjs changed to ClipboardJS
-                var clipboard = new ClipboardJS(element[0]);
-
-                clipboard.on('success', function(e) {
-                  scope.$apply(function () {
-                    scope.ngclipboardSuccess({
-                      e: e
+                    clipboard.on('success', function(e) {
+                      scope.$apply(function () {
+                        scope.ngclipboardSuccess({
+                          e: e
+                        });
+                      });
                     });
-                  });
-                });
 
-                clipboard.on('error', function(e) {
-                  scope.$apply(function () {
-                    scope.ngclipboardError({
-                      e: e
+                    clipboard.on('error', function(e) {
+                      scope.$apply(function () {
+                        scope.ngclipboardError({
+                          e: e
+                        });
+                      });
                     });
-                  });
-                });
 
-                element.on('$destroy', function() {
-                    clipboard.destroy();
-                });
+                    element.on('$destroy', function() {
+                        clipboard.destroy();
+                    });
 
-            }
-        };
-    });
-})();
+                }
+            };
+        });
+
+        return ngModule;
+    },
+};
+
+export {
+    ngclipboard
+};
+
